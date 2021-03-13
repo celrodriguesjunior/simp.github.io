97a4371164c6aaa5c4c1b6c2689cd4d5c77dbdd1
HenriqueJW <henriquewilbert123@gmail.com>
2021-03-13 13:52:46 -0300

    1

--- js/requests.js
+++ js/requests.js
@@ -254,7 +254,7 @@ function postUsuario(usuario) {
 
     $.ajax({
         url: "http://simprestapi.ddns.net:5000/v1/usuario", type: "POST", data:
-            JSON.stringify(universitario), success: function (result) {
+            JSON.stringify(usuario), success: function (result) {
                 alert("Cadastro feito com sucesso!")
             }, contentType: "application/json"
     });
