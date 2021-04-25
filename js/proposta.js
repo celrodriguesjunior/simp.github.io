e05e6eb9376d805dafdec5e75a872d2447583384
celrodriguesjunior <rodriguesjuniorcelio@gmail.com>
2021-04-25 16:03:53 -0300

    compartilhar

--- js/proposta.js
+++ js/proposta.js
@@ -288,3 +288,18 @@ function retornaInstituicao(resp) {
 
 
 }
+
+
+
+
+function share(){
+	if (navigator.share !== undefined) {
+		navigator.share({
+			title: 'O título da sua página',
+			text: 'Um texto de resumo',
+			url: 'https://seusite.com/sua_url',
+		})
+		.then(() => console.log('Successful share'))
+		.catch((error) => console.log('Error sharing', error));
+	}
+}
