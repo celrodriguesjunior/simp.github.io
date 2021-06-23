c27cdbf88a7619f038d45d70ebb07c05c1a6a4f3
HenriqueJW <henriquewilbert123@gmail.com>
2021-06-22 21:05:10 -0300

    Faq

--- js/BCKgeral.js
+++ js/BCKgeral.js
@@ -17,16 +17,9 @@ $(document).ready(function () {
         }
 
         $('#menusCabecalho').append($('<li class="nav-item"><a class="nav-link link text-black display-4" onClick="logout()">Logout</a></li>'))
-    } else {
+    }else{
         $('#menusCabecalho').append($('<li class="nav-item"><a class="nav-link link text-black display-4" href="login.html"">Login</a></li>'))
     }
-
-    var rodape = $('<div class="col-12 col-md-6 col-lg-3 rodape">')
-    var textRodape = $('<h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-7" style="color: black;">')
-    var link = $('<a href="faq.html"><strong style="color:black;">Dúvidas? <br> Visite nosso FAQ clicando aqui!</strong></a>')
-    textRodape.append(link)
-    rodape.append(textRodape)
-    $('.footer4').find('.row.mbr-white').append(rodape)
 
 })
 
